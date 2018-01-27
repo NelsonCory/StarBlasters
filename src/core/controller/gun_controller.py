85adcd8bbf36803f6ffd79cf7b673c4dd7045c9a
DucksCantFly <34819470+DucksCantFly@users.noreply.github.com>
2018-01-27 11:39:53 -0600

    Add files via upload

--- src/core/controller/gun_controller.py
+++ src/core/controller/gun_controller.py
@@ -6,3 +6,8 @@ class GunController(Controller):
 
 	def __init__(self):
 		super(GunController, self).__init__()
+
+		#set up joystick
+		self.__joystick = pygame.joystick.Joystick(0) #first joystick
+		self.__joystick.init()
+		self.__axes = self.__joystick.get_numaxes()
