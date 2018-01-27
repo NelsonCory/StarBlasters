4fc8a4613beb87b194b8042adda7e0ab05a0dab2
Christopher Clouse <cloostefismt880@gmail.com>
2018-01-27 01:05:15 -0600

    Implement Scene.draw().

--- src/gui/scene.py
+++ src/gui/scene.py
@@ -11,3 +11,13 @@ class Scene:
 			e_rect = e.get_rect()
 			if camera_rect.collide(e_rect):
 				e.draw(screen, camera_rect.left, camera_rect.top)
+
+	def tick(self, dt):
+		scale_factor = 3
+		camera_rect = self.__camera.get_rect()
+		process_rect = camera_rect.inflate(scale_factor, scale_factor)
+		for e in self.__entities:
+			e_rect = e.get_rect()
+			if process_rect.collide(e_rect):
+				e.tick(dt)
+
