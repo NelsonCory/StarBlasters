a6f64ca3ece842b9ff5f3705353c7b2aef0c65d7
Christopher Clouse <cloostefismt880@gmail.com>
2018-01-26 23:53:02 -0600

    Fix obvious bug in event manager.

--- src/core/event_manager.py
+++ src/core/event_manager.py
@@ -11,7 +11,7 @@ class EventManager:
 		while len(self.__queue) > 0:
 			label, value = self.__queue.pop()
 			if label in self.__callbacks:
-				for f in self.__callbacks:
+				for f in self.__callbacks[label]:
 					f(value)
 
 	def subscribe(self, label, callback):
