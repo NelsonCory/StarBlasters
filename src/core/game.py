8d4e4d9f7d8ee9ab6ae8979a4eb166fbc2a07a64
David Ludwig <davidludwigii@gmail.com>
2018-01-26 22:57:37 -0600

    Main loop set up

--- src/core/game.py
+++ src/core/game.py
@@ -1,13 +1,25 @@
+import pygame
 
 
 class Game():
 
 	# Initialize pygame here
 	def __init__(self):
-		pass
+		pygame.init()
+
+		self.__done = False
+		self.__clock = pygame.time.Clock()
 
 	# Main loop, returns exit code
 	def run(self):
 		# Main loop
-		print("Success!")
+		while not self.__done:
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					self.__done = True
+			dt = self.__clock.tick(60) / 1000.0
 		return 0
+
+
+	def exit(self):
+		self.__done = True
