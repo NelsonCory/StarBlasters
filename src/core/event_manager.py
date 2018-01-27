69a21bf7a6cda1a18ff34b2086c2fc0cd8ca1355
Christopher Clouse <cloostefismt880@gmail.com>
2018-01-26 23:50:47 -0600

    Add event manager.

---
+++ src/core/event_manager.py
@@ -0,0 +1,22 @@
+class EventManager:
+
+	def __init__(self):
+		self.__queue = []
+		self.__callbacks = {}
+
+	def send(self, label, value):
+		self.__queue.insert(0, (label, value))
+
+	def dispatch(self):
+		while len(self.__queue) > 0:
+			label, value = self.__queue.pop()
+			if label in self.__callbacks:
+				for f in self.__callbacks:
+					f(value)
+
+	def subscribe(self, label, callback):
+		if label in self.__callbacks:
+			self.__callbacks[label].append(callback)
+		else:
+			self.__callbacks[label] = [callback]
+
