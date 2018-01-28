52db75a994bdd5d4324595a16e62a439ad11ea67
DucksCantFly <34819470+DucksCantFly@users.noreply.github.com>
2018-01-27 19:32:38 -0600

    Add files via upload

--- src/gui/scene/main_menu.py
+++ src/gui/scene/main_menu.py
@@ -1,6 +1,10 @@
 from . scene import *
+from core.controller.menu_controller import *
 
 class MainMenu(Scene):
 
 	def __init__(self):
 		super(MainMenu, self).__init__()
+		self.__menu_controller = MenuController()
+
+	
