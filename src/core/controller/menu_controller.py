2f3329fca9283f34a41bd9492eae97b18e20b08d
DucksCantFly <34819470+DucksCantFly@users.noreply.github.com>
2018-01-27 19:28:09 -0600

    Add files via upload

---
+++ src/core/controller/menu_controller.py
@@ -0,0 +1,46 @@
+from core.event_manager import *
+import pygame
+
+class MenuController():
+
+	def __init__(self):
+		super(MenuController, self).__init__()
+
+		#set up joystick 1
+		try:
+			self.__joystick = pygame.joystick.Joystick(0) #first joystick
+			self.__joystick.init()
+			self.__buttons = self.__joystick.get_button(3)
+		except:
+			print("ERROR: NOT ENOUGH JOYSTICKS - ShipController")
+		
+		#set up joystick 2
+		try:
+			self.__joystick2 = pygame.joystick.Joystick(1)
+			self.__joystick2.init()
+			self.__buttons2 = self.__joystick2.get_button(3)
+
+	def key_release(self, event):
+		#go to world, keyboard command
+		if event.key == pygame.K_SPACE:
+			pass
+		
+		
+	def update(self):
+		if not self.__dirty:
+			return
+
+	def receive_joy(self):
+		#joystick2
+		try:
+			if(self.__buttons[]):
+				pass
+		except:
+			print("ERROR - menuController, receive view")
+		#joystick2
+		try:
+			if(self.__buttons2[0]):
+				return self.__buttons2[0]
+		except:
+			pass
+			
