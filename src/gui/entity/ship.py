a33c5ec9d6fa17552d19d13a1c812a1dcb0c286d
Christopher Clouse <cloostefismt880@gmail.com>
2018-01-27 18:18:20 -0600

    Begin work on the gun.

--- src/gui/entity/ship.py
+++ src/gui/entity/ship.py
@@ -14,10 +14,10 @@ class Ship(Entity):
 		self.set_position((0, 0))
 		self.__acceleration = (0, 0)
 		self.__velocity = (0, 0)
-		self.__gun_rot = 0
+		self.__gun_rot = 3*math.pi/2
 		self.__resource_manager = ResourceManager.get_instance()
 		self.set_texture(self.__resource_manager.get_image("graphics/mothership"))
-		self.__gun_image = self.__resource_manager.get_image("graphics/gun")
+		self.__gun_image = self.__resource_manager.get_image("graphics/mothership_gun")
 		self.__glow1 = self.__resource_manager.get_image("graphics/mothership_brightness_2")
 		EventManager.get_instance().subscribe("ship_move", self.on_accelerate)
 
@@ -30,6 +30,13 @@ class Ship(Entity):
 		pos = add_vecs(self.get_position(), (cx, cy))
 		screen.blit(self.get_texture(), pos)
 		screen.blit_alpha(self.__glow1, pos, (math.sin(self.__glow_phase)/2+0.5)*255)
+
+		ship_radius = self.get_texture().get_rect().width / 2
+		gun_radius = ship_radius + 20
+		gun_center = scale_vec(gun_radius, (math.cos(self.__gun_rot), math.sin(self.__gun_rot)))
+		rotated_gun = pygame.transform.rotate(self.__gun_image, self.__gun_rot - math.pi/2)
+		gun_loc = add_vecs(gun_center, scale_vec(0.5, self.__gun_image.get_rect().size))
+		screen.blit(self.__gun_image, gun_loc)
 
 	def on_accelerate(self, delta):
 		self.__acceleration = delta
