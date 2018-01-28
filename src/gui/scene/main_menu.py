0bd3e2e43daa41c4b8c01c8040659199ebfb0bbd
DucksCantFly <34819470+DucksCantFly@users.noreply.github.com>
2018-01-27 19:54:41 -0600

    Add files via upload

--- src/gui/scene/main_menu.py
+++ src/gui/scene/main_menu.py
@@ -6,5 +6,6 @@ class MainMenu(Scene):
 	def __init__(self):
 		super(MainMenu, self).__init__()
 		self.__menu_controller = MenuController()
-
+		self.add_controller(self.__menu_controller)
+		self.set_camera(Camera())
 	
