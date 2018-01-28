f64e7deb53d4cd4ae5e911aaf8df93c0f24c184d
Bryce <baault@gmail.com>
2018-01-27 18:28:11 -0600

    score manager

---
+++ src/core/score_manager.py
@@ -0,0 +1,36 @@
+class ScoreDatabase():
+
+    def __init__(self, path):
+        self.__path = path
+        self.__scores = []
+        # self.__currScore = 0
+    
+    def load():
+        self.__file = open(self.__path, 'r')
+        self.__scores = []
+        for line in self.__file:
+            self.__scores.append(int(line))
+        self.__file.close()
+
+    def save():
+        self.__file = open(self.__path, 'w')       
+        for score in self.__scores:
+            self.__file.write(score)
+
+    def add(score):
+        # self.__currScore = score
+        #   if less than 5 scores exist then add score
+        if self.__scores.len() < 5:
+            self.__scores.append(score)
+        #   else check smallest value and remove if score > minimum
+        else:
+            minimum = min(self.__scores)
+            if minimum < score:
+                self.__scores.remove(minimum)
+                self.__scores.append(score)
+        #   sort scores
+        list(reversed(sorted(self.__scores)))
+
+    def get_scoreDatabase():
+        return self.__scores
+
